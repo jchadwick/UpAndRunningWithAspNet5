4e903d40abc296e109c7443abb15b271db8a0502
Jess Chadwick <jesschadwick@gmail.com>
2015-05-26 03:04:33 -0400

    Adding ASP.NET MVC

--- AspNetBlog/src/AspNetBlog/Startup.cs
+++ AspNetBlog/src/AspNetBlog/Startup.cs
@@ -13,10 +13,13 @@ namespace AspNetBlog
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         public void Configure(IApplicationBuilder app)
         {
+            app.UseMvc(routes => routes.MapRoute(
+                "Default", "{controller=Home}/{action=Index}/{id?}"));
             app.UseFileServer();
         }
     }
