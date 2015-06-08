e43036221794681fb5bc6c0978800e19f740541e
Jess Chadwick <jesschadwick@gmail.com>
2015-06-08 00:29:32 -0400

    Adding Entity Framework

---
+++ AspNetBlog/src/AspNetBlog/Models/BlogDataContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetBlog.Models
+{
+    public class BlogDataContext : DbContext
+    {
+        public DbSet<Post> Posts { get; set; }
+
+        public BlogDataContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            var connectionString = @"Server=(LocalDb)\MSSQLLocalDb;Database=AspNetBlog";
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ForSqlServer().UseIdentity();
+        }
+    }
+}
--- AspNetBlog/src/AspNetBlog/Models/Post.cs
+++ AspNetBlog/src/AspNetBlog/Models/Post.cs
@@ -8,6 +8,8 @@ namespace AspNetBlog.Models
 {
     public class Post
     {
+        public long Id { get; set; }
+
         [Required]
         [DataType(DataType.Text)]
         [Display(Name = "Blog post title")]
