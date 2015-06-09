676d14d1104562c8b627f89868b407227c5ff826
Jess Chadwick <jesschadwick@gmail.com>
2015-06-09 02:52:26 -0400

    Adding Identity Services and model classes

---
+++ AspNetBlog/src/AspNetBlog/Models/Identity/IdentityDataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetBlog.Models.Identity
+{
+    public class IdentityDataContext : IdentityDbContext<ApplicationUser>
+    {
+    }
+}
