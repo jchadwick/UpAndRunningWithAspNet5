f6a5417eb79ed2fb3979e576fa0bc682508d545a
Jess Chadwick <jesschadwick@gmail.com>
2015-06-15 01:35:48 -0400

    Adding Partial View

--- AspNetBlog/src/AspNetBlog/Controllers/HomeController.cs
+++ AspNetBlog/src/AspNetBlog/Controllers/HomeController.cs
@@ -14,7 +14,28 @@ namespace AspNetBlog.Controllers
         // GET: /<controller>/
         public IActionResult Index()
         {
-            return View();
+            var posts = new[] {
+                new Post {
+                    Title = "Blog Post #1",
+                    PostedDate = DateTime.Now,
+                    Author = "Jess Chadwick",
+                    Body = "This is my first blog post about ASP.NET MVC 6!",
+                },
+                new Post {
+                    Title = "Blog Post #2",
+                    PostedDate = DateTime.Now,
+                    Author = "Jess Chadwick",
+                    Body = "This is my second blog post about ASP.NET MVC 6!",
+                },
+                new Post {
+                    Title = "Blog Post #3",
+                    PostedDate = DateTime.Now,
+                    Author = "Jess Chadwick",
+                    Body = "This is my third blog post about ASP.NET MVC 6!",
+                }
+            };
+
+            return View(posts);
         }
 
         public IActionResult Post(long id)
