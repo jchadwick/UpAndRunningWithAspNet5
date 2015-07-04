5f61d4fc4497ce1554582eb3f5b469bac0a847b1
Jess <jesschadwick@gmail.com>
2015-07-04 02:02:30 -0400

    Adding new Model, View, and Controller to prepare for user account registration

---
+++ AspNetBlog/src/AspNetBlog/Controllers/AccountController.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using AspNetBlog.Models.Account;
+using AspNetBlog.Models.Identity;
+
+namespace AspNetBlog.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View(new RegisterViewModel());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel registration)
+        {
+            if (!ModelState.IsValid)
+                return View(registration);
+
+            var newUser = new ApplicationUser
+            {
+                Email = registration.EmailAddress,
+                UserName = registration.EmailAddress,
+            };
+
+            // TODO: Add new user to the system
+        }
+
+    }
+}
---
+++ AspNetBlog/src/AspNetBlog/Models/Account/RegisterViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNetBlog.Models.Account
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress, MaxLength(500)]
+        [Display(Name = "Email Address")]
+        public string EmailAddress { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "Passwords must match")]
+        [Display(Name = "Confirm Password")]
+        public string ConfirmPassword { get; set; }
+    }
+
+}
