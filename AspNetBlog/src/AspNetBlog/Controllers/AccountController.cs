2e54383d501449ba8076d5c2ff8d0bdf3b1bcb6e
Jess Chadwick <jesschadwick@gmail.com>
2015-07-04 02:25:28 -0400

    Implement UserManager.Create

--- AspNetBlog/src/AspNetBlog/Controllers/AccountController.cs
+++ AspNetBlog/src/AspNetBlog/Controllers/AccountController.cs
@@ -2,11 +2,19 @@
 using Microsoft.AspNet.Mvc;
 using AspNetBlog.Models.Account;
 using AspNetBlog.Models.Identity;
+using Microsoft.AspNet.Identity;
 
 namespace AspNetBlog.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
         [HttpGet]
         public IActionResult Register()
         {
@@ -25,7 +33,15 @@ namespace AspNetBlog.Controllers
                 UserName = registration.EmailAddress,
             };
 
-            // TODO: Add new user to the system
+            var result = await _userManager.CreateAsync(newUser, registration.Password);
+
+            if (!result.Succeeded)
+            {
+                ModelState.AddModelError("", "Error creating user");
+                return View(registration);
+            }
+
+            return RedirectToAction("Login");
         }
 
     }
